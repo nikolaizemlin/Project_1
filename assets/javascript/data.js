e5bd0267e4d357da44151b35e32a940e8e82a098
Nick Hosley <nhosley1976@gmail.com>
2018-06-16 08:36:23 -0700

    added firebase

---
+++ assets/javascript/data.js
@@ -0,0 +1,12 @@
+  
+  
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDXEMDXUtKYjOJoZoIwF-ZbKbg_yROcGUs",
+    authDomain: "project1-4b843.firebaseapp.com",
+    databaseURL: "https://project1-4b843.firebaseio.com",
+    projectId: "project1-4b843",
+    storageBucket: "project1-4b843.appspot.com",
+    messagingSenderId: "612612974443"
+  };
+  firebase.initializeApp(config);
