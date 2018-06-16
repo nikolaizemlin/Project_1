12d50e022fcfd5f79f4bf017f640250d5c5cbdd6
Nick Hosley <nhosley1976@gmail.com>
2018-06-16 11:06:27 -0700

    added code

--- assets/javascript/data.js
+++ assets/javascript/data.js
@@ -1,12 +1,50 @@
+
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDXEMDXUtKYjOJoZoIwF-ZbKbg_yROcGUs",
+  authDomain: "project1-4b843.firebaseapp.com",
+  databaseURL: "https://project1-4b843.firebaseio.com",
+  projectId: "project1-4b843",
+  storageBucket: "project1-4b843.appspot.com",
+  messagingSenderId: "612612974443"
+};
+firebase.initializeApp(config);
+
+var dataRef = firebase.database();
+
+// Initial Values
+var fname = "";
+var lname = "";
+var email = "";
+var password = "";
+
+// Capture Button Click
+$("#add-person").on("click", function() {
+
+  fname = $("#fname-input").val().trim();
+  lname = $("#lname-input").val().trim();
+  email = $("#email-input").val().trim();
+  password = $("#password-input").val().trim();
+
+  // Push
+  dataRef.ref().push({
+
+    fname: fname,
+    lname: lname,
+    email: email,
+    password: password,
   
-  
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDXEMDXUtKYjOJoZoIwF-ZbKbg_yROcGUs",
-    authDomain: "project1-4b843.firebaseapp.com",
-    databaseURL: "https://project1-4b843.firebaseio.com",
-    projectId: "project1-4b843",
-    storageBucket: "project1-4b843.appspot.com",
-    messagingSenderId: "612612974443"
-  };
-  firebase.initializeApp(config);
+  )}; 
+  console.log(add-person);
+    // clear text
+$("#fname-input").val("");
+$("#lname-input").val("");
+$("#email-input").val("");
+$("#password-input").val("");
+
+   // No refresh
+  return false;
+ 
+  )}; 
+
