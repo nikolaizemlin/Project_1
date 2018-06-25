0c01683337bbd254886de8a6e3c7429f67e395d2
DanielG <Danielmorrisg@gmail.com>
2018-06-24 18:41:09 -0700

    added authorization to main.html

--- spotify-testing/testing.js
+++ spotify-testing/testing.js
@@ -11,19 +11,20 @@ const server = http.createServer((req, res) => {
         });
     } 
     else {
-        res.end(`
-            <!doctype html>
-            <html>
-            <body>
-                <form action="/" method="post">
-                    <input type="text" name="fname" /><br />
-                    <input type="number" name="age" /><br />
-                    <input type="file" name="photo" /><br />
-                    <button>Save</button>
-                </form>
-            </body>
-            </html>
-        `);
+        console.log("Else")
+        // res.end(`
+        //     <!doctype html>
+        //     <html>
+        //     <body>
+        //         <form action="/" method="post">
+        //             <input type="text" name="fname" /><br />
+        //             <input type="number" name="age" /><br />
+        //             <input type="file" name="photo" /><br />
+        //             <button>Save</button>
+        //         </form>
+        //     </body>
+        //     </html>
+        // `);
     }
 });
 server.listen(3000);
